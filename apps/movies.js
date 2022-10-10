c50946dcdea8cf8d6a37a3ecc8960f1481301a79
James <knotsite@gmail.com>
2022-10-10 15:07:00 +0700

    feat: add update and delete

--- apps/movies.js
+++ apps/movies.js
@@ -1,3 +1,4 @@
+import { ObjectId } from "mongodb";
 import { Router } from "express";
 
 // 1) Import ตัว Database ที่สร้างไว้มาใช้งาน
@@ -30,6 +31,49 @@ movieRouter.post("/", async (req, res) => {
   // 4) Return ตัว Response กลับไปหา Client
   return res.json({
     message: `Movie record (${movies.insertedId}) has been created successfully`,
+  });
+});
+
+movieRouter.put("/:movieId", async (req, res) => {
+  // 2) เลือก Collection ที่ชื่อ `movies`
+  const collection = db.collection("movies");
+
+  // 3) Update ข้อมูลใน Database โดยใช้ `collection.updateOne(query)` โดยการ
+  // นำ movieId จาก Endpoint parameter มา Assign ลงใน Variable `movieId` โดยที่ใช้ ObjectId ในการ Convert Type ก่อน
+  const movieId = ObjectId(req.params.movieId);
+  // นำข้อมูลที่ส่งมาใน Request Body ทั้งหมด Assign ใส่ลงไปใน Variable ที่ชื่อว่า `newMovieData`
+  const newMovieData = { ...req.body };
+
+  await collection.updateOne(
+    {
+      _id: movieId,
+    },
+    {
+      $set: newMovieData,
+    }
+  );
+
+  // 4) ส่ง Response กลับไปหา Client
+  return res.json({
+    message: `Movie record (${movieId}) has been updated successfully`,
+  });
+});
+
+movieRouter.delete("/:movieId", async (req, res) => {
+  // 2) เลือก Collection ที่ชื่อ `movies`
+  const collection = db.collection("movies");
+
+  // 3) Delete ข้อมูลออกจากใน Database โดยใช้ `collection.deleteOne(query)`
+  // นำ movieId จาก Endpoint parameter มา Assign ลงใน Variable `movieId`
+  const movieId = ObjectId(req.params.movieId);
+
+  await collection.deleteOne({
+    _id: movieId,
+  });
+
+  // 4) ส่ง Response กลับไปหา Client
+  return res.json({
+    message: `Movie record (${movieId}) has been deleted successfully`,
   });
 });
 
