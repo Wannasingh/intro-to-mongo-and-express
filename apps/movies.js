e9510adb5b4a914be122133ae9be36d0f075a26a
James <knotsite@gmail.com>
2022-10-10 14:56:19 +0700

    feat: get movies

--- apps/movies.js
+++ apps/movies.js
@@ -1,11 +1,22 @@
 import { Router } from "express";
 
+// 1) Import ตัว Database ที่สร้างไว้มาใช้งาน
+import { db } from "../utils/db.js";
+
 const movieRouter = Router();
 
 movieRouter.get("/", async (req, res) => {
-  return res.json({
-    message: "Movie data",
-  });
+  // 2) เลือก Collection ที่ชื่อ `movies`
+  const collection = db.collection("movies");
+
+  // 3) เริ่ม Query โดยใช้ `collection.find(query)`
+  const movies = await collection
+    .find({ year: 2008 })
+    .limit(10) // limit the result documents by 10
+    .toArray(); // convert documents into an array
+
+  // 4) Return ตัว Response กลับไปหา Client
+  return res.json({ data: movies });
 });
 
 export default movieRouter;
